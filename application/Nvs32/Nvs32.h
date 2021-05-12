ac7332c0b0fd4b7d4e19b07973ae4d8a88f8ca32
Simon <Simon@DESKTOP-JGL5HTQ>
2021-05-12 16:11:49 +0100

    Video 11

--- application/Nvs32/Nvs32.h
+++ application/Nvs32/Nvs32.h
@@ -25,26 +25,32 @@ public:
 
     // With respect to a type
     template <typename T>
-    [[nodiscard]] esp_err_t get(const char* const key, T& output);
+    [[nodiscard]] esp_err_t get(const char* const key, T& output)
+        { return _get_buf(handle, key, output, 1); }
 
     template <typename T>
-    [[nodiscard]] esp_err_t set(const char* const key, const T input);
+    [[nodiscard]] esp_err_t set(const char* const key, const T input)
+        { return _set_buf(handle, key, input, 1); }
 
     template <typename T>
-    [[nodiscard]] esp_err_t verify(const char* const key, const T input);
+    [[nodiscard]] esp_err_t verify(const char* const key, const T input)
+        { return _verify_buf(handle, key, input, 1); }
 
 
     // With respect to a buffer
     template <typename T>
-    [[nodiscard]] esp_err_t get_buffer(const char* const key, T* output, const size_t len);
+    [[nodiscard]] esp_err_t get_buffer(const char* const key, T* output, size_t& len)
+        { return _get_buf(handle, key, output, len); }
 
     template <typename T>
-    [[nodiscard]] esp_err_t set_buffer(const char* const key, const T input, const size_t len);
+    [[nodiscard]] esp_err_t set_buffer(const char* const key, const T* input, const size_t len)
+        { return _set_buf(handle, key, input, len); }
 
     template <typename T>
-    [[nodiscard]] esp_err_t verify_buffer(const char* const key, const T input, const size_t len);
+    [[nodiscard]] esp_err_t verify_buffer(const char* const key, const T* input, const size_t len)
+        { return _verify_buf(handle, key, input, len); }
 
-protected:
+private:
     [[nodiscard]] static esp_err_t _open(const char* const partition_name, nvs_handle_t& handle)
         { return nvs_open(partition_name, NVS_READWRITE, &handle); }
 
@@ -79,7 +85,7 @@ protected:
     }
 
     template <typename T>
-    [[nodiscard]]static esp_err_t _verify(nvs_handle_t handle, const char* const key, T& input)
+    [[nodiscard]] static esp_err_t _verify(nvs_handle_t handle, const char* const key, T& input)
     {
         T val_in_nvs{};
         esp_err_t status = _get(handle, key, val_in_nvs);
@@ -91,6 +97,82 @@ protected:
                 return ESP_FAIL;
         else
             return status;
+    }
+
+    // Note, length of num items of type T (not necessarily num bytes)
+    template <typename T>
+    [[nodiscard]] static esp_err_t _get_buf(nvs_handle_t handle, const char* const key, 
+                                            T* output, size_t& len)
+    {
+        size_t n_bytes{sizeof(T) * len};
+
+        if (nullptr == key || 0 == strlen(key) || nullptr == output || 0 == len)
+            return ESP_ERR_INVALID_ARG;
+        else
+        {
+            const esp_err_t status = nvs_get_blob(handle, key, output, &n_bytes);
+
+            len = n_bytes / sizeof(T);
+
+            return status;
+        }
+    }
+
+    template <typename T>
+    [[nodiscard]] static esp_err_t _set_buf(nvs_handle_t handle, const char* const key, 
+                                            const T* input, const size_t len)
+    {
+        size_t n_bytes{sizeof(T) * len};
+        esp_err_t status{ESP_OK};
+
+        if (nullptr == key || 0 == strlen(key) || nullptr == input || 0 == len)
+            status = ESP_ERR_INVALID_ARG;
+        else
+        {
+            status = nvs_set_blob(handle, key, input, &n_bytes);
+
+            if (ESP_OK == status)
+                status = nvs_commit(handle);
+
+            if (ESP_OK == status)
+                status = _verify_buf(handle, key, input, len);
+        }
+
+        return status;
+    }
+
+    template <typename T>
+    [[nodiscard]] esp_err_t _verify_buf(nvs_handle_t handle, const char* const key, 
+                                        const T* input, const size_t len)
+    {
+        esp_err_t status{ESP_OK};
+
+        T* buf_in_nvs = new T[len]{};
+        size_t n_items_in_nvs = len;
+
+        if (buf_in_nvs)
+        {
+            status = _get_buf(handle, key, buf_in_nvs, n_items_in_nvs);
+
+            if (ESP_OK == status)
+            {
+                if (len == n_items_in_nvs)
+                {
+                    if (0 != memcmp(input, buf_in_nvs, len * sizeof(T)))
+                        status = ESP_FAIL;
+                }
+                else
+                    status = ESP_ERR_NVS_INVALID_LENGTH;
+            }
+
+            delete[] buf_in_nvs;
+        }
+        else
+        {
+            status = ESP_ERR_NO_MEM;
+        }
+
+        return status;
     }
 
 };
